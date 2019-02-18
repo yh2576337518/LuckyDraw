fb6b389a26c8a2511680100263b4dfd8d6bb5167
舌尖血 <2576337518@qq.com>
2019-02-18 17:27:43 +0800

    Add files via upload

---
+++ LuckyDraw/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LuckyDraw
+//
+//  Created by apple on 2019/1/17.
+//  Copyright © 2019年 dywc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LuckyDraw/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LuckyDraw
+//
+//  Created by apple on 2019/1/17.
+//  Copyright © 2019年 dywc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
